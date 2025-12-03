22783b3369bea43b82df3808f5e5d16f382ef53c
Gecsan <greyes2054@sdsu.edu>
2025-12-03 12:00:00 -0800

    updated code

--- led_button.py
+++ led_button.py
@@ -22,18 +22,11 @@ def main() -> None:
     GPIO.setup(LED_PIN, GPIO.OUT, initial=GPIO.LOW)
     GPIO.setup(BUTTON_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-    print(
-        f"Watching button on pin {BUTTON_PIN} -> driving LED on pin {LED_PIN}. "
-        "Press Ctrl+C to exit."
-    )
-
     try:
         while True:
             pressed = GPIO.input(BUTTON_PIN) == GPIO.LOW
             GPIO.output(LED_PIN, GPIO.HIGH if pressed else GPIO.LOW)
             time.sleep(0.02)  # debounce-ish
-    except KeyboardInterrupt:
-        pass
     finally:
         GPIO.output(LED_PIN, GPIO.LOW)
         GPIO.cleanup()
